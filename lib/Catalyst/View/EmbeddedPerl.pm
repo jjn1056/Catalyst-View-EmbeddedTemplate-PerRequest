a657cb15fb12a4f8af872f2504f0384a644a0d50
John Napiorkowski <jjn1056@yahoo.com>
2024-09-25 17:30:42 -0500

    docs

--- lib/Catalyst/View/EmbeddedPerl.pm
+++ lib/Catalyst/View/EmbeddedPerl.pm
@@ -1,6 +1,5 @@
 package Catalyst::View::EmbeddedPerl;
 
-
 our $VERSION = 0.001002;
 eval $VERSION;
 
@@ -11,8 +10,11 @@ Catalyst::View::EmbeddedPerl - Views using Template::EmbeddedPerl
 
 =head1 DESCRIPTION
 
-This is a placeholder for the distribution.
-See L<Catalyst::View::EmbeddedPerl::PerRequest>.
+For now this view is only available as a PerRequest view, so see
+See L<Catalyst::View::EmbeddedPerl::PerRequest> for more.
+
+This is a placeholder in case I want to release a 'traditional' per application
+version of this view.  
 
 
 =head1 AUTHOR
